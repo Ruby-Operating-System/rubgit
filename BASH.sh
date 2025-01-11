c332b59e93b0d98d3196f8a7437aba969902371d
NovaOSholder <bennuribaykondu5@gmail.com>
2025-01-11 23:44:20 +0300

    Create BASH.sh

---
+++ BASH.sh
@@ -0,0 +1,15 @@
+echo RUBGIT IS RUNNING, PLEASE MAKE SURE THAT GIT IS INSTALLED.
+
+git clone https://github.com/Ruby-Operating-System/ruby-open-source-shell-1.5
+
+git clone https://github.com/Ruby-Operating-System/ruby-open-source-shell
+
+git clone https://github.com/Ruby-Operating-System/src
+
+git clone https://github.com/Ruby-Operating-System/disk
+
+git clone https://github.com/Ruby-Operating-System/rootbin
+
+git clone https://github.com/Ruby-Operating-System/ui
+
+git clone https://github.com/Ruby-Operating-System/root
